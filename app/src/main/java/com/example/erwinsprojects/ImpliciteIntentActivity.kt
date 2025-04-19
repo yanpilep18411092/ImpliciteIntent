8dd3496546e658da12200c5c794a0152528fd6d1
Yan Pilep <yanpilep18411092@gmail.com>
2025-04-19 20:56:11 +0900

    commit 2 implicite intent rev 1

--- app/src/main/java/com/example/erwinsprojects/ImpliciteIntentActivity.kt
+++ app/src/main/java/com/example/erwinsprojects/ImpliciteIntentActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.erwinsprojects.databinding.ActivityExplicitIntentBinding
 import com.example.erwinsprojects.databinding.ActivityImpliciteIntentBinding
 
 class ImpliciteIntentActivity : AppCompatActivity() {
